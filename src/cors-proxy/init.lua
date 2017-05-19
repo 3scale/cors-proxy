8be6d395f69947116413b730d7d599c1ccefa24a
Michal Cichra <michal@cichra.cz>
2017-05-19 11:10:25 +0200

    basic project structure

--- src/cors-proxy/init.lua
+++ src/cors-proxy/init.lua
@@ -1,5 +1,26 @@
+local balancer = require('cors-proxy.balancer_blacklist')
+
 local _M = {
-  _VERSION = '0.1'
+  _VERSION = '0.1',
+  balancer = balancer.new()
 }
+
+function _M:init()
+  local balancer = self.balancer
+
+  if not balancer then
+    return nil, 'not initialized'
+  end
+
+  balancer:init()
+end
+
+function _M:access()
+  -- TODO: verify domain whitelist
+end
+
+function _M:content()
+  -- TODO: resolve and proxy to the correct server
+end
 
 return _M
